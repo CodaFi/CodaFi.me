88e757215eb8f2621260a265c7f8f44e55726d15
CodaFi <devteam.codafi@gmail.com>
2013-10-23 17:36:54 -0600

    Attempt to fix skeleton

--- _elm/Website/Blog/Skeleton.elm
+++ _elm/Website/Blog/Skeleton.elm
@@ -9,7 +9,7 @@ generatePost (w,h) t postBody = let bodyHeight = fst <| sizeOf postBody
                                 in flow down [ container (max 500 w) 60 (midLeftAt (absolute 10) (absolute 30)) (Fmt.title "  CF") |> color (rgb 57 74 85),
                                                spacer (max 300 w) 30,
                                                container (max 300 w) 60 middle (Fmt.formatTitleText t),                             
-                                               container w h midTop (size (min 550 w) (max bodyHeight h) postBody)
+                                               container w (bodyHeight + 120) midTop (size (min 550 w) (max bodyHeight h) postBody)
                                              ] 
 
 generateImagefulPost : (Int, Int) -> String -> String -> Element -> Element
@@ -17,5 +17,5 @@ generateImagefulPost (w,h) t im postBody = let bodyHeight = fst <| sizeOf postBo
                                            in flow down [ container (max 300 w) 60 midLeft (Fmt.formatImagefulTitle (w, 60) "  CF" im) |> color (rgb 57 74 85),
                                                           spacer (max 300 w) 30,
                                                           container (max 300 (w + 20)) 60 middle (Fmt.formatTitleText t),                             
-                                                          container w h midTop (size (min 550 w) (max bodyHeight h) postBody)
+                                                          container w (bodyHeight + 120) midTop (size (min 550 w) (max bodyHeight h) postBody)
                                                         ]
