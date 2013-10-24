b72712bd927f569f5d911952dd31ddaf2ebe4afb
CodaFi <devteam.codafi@gmail.com>
2013-10-23 18:31:38 -0600

    Spellin' n' Grammrz n' Linkz

--- _elm/src/blogs/INTERCAL.elm
+++ _elm/src/blogs/INTERCAL.elm
@@ -6,17 +6,17 @@ post = [markdown|
 <p>
 ## Introduction
 
-I have been writing Objective-C for the entirety of my (arguably short) programming career, and the only conclusion I can draw from all that time spent in the world of square-brackets and verbosity is that it forces me to think in extremely provincially about my code.  While the language itself is founded on very powerful ideas, and the libraries and tools provided by Apple’s walled garden are of an agreeable quality, there was always an urge within me to "peek out over the garden walls”, as it were, and experiment with other languages.  I’ve dabbled in the web, and left with a sour taste.  I’ve poked and prodded the list comprehension languages, forced the functional ones, and written self-reorganizing code in the ones promising to satisfy that code-as-data fetish for those of us so inclined that way.  But, paradoxically enough, the esoteric languages have, by far, been the most educational and satisfying.  To be fair, I’m not saying that you should run out and immediately install Malbolge on your computer -just to watch your precious RAM fill with garbage and have to program entirely in random, cryptographically-restructured, pointer arithmetic- but rather that it is those languages that seem most foreign that often have the most to offer.  It is in this spirit that I am going to discuss one of my great loves: INTERCAL.
+I have been writing Objective-C for the entirety of my (arguably short) programming career, and the only conclusion I can draw from all that time spent in the world of square-brackets and verbosity is that it forces me to think in extremely provincially about my code.  While the language itself is founded on very powerful ideas, and the libraries and tools provided by Apple’s walled garden are of an agreeable quality, there was always an urge within me to "peek out over the garden walls”, as it were, and experiment with other languages.  I’ve dabbled in the web, and left with a sour taste.  I’ve poked and prodded the list comprehension languages, forced the functional ones, and written self-reorganizing code in the ones promising to satisfy that code-as-data fetish for those of us so inclined that way.  But, paradoxically enough, the esoteric languages have, by far, been the most educational and satisfying.  To be fair, I’m not saying that you should run out and immediately install [Malbolge](http://en.wikipedia.org/wiki/Malbolge) on your computer -just to watch your precious RAM fill with garbage and have to program entirely in random, cryptographically-restructured, pointer arithmetic- but rather that it is those languages that seem most foreign that often have the most to offer.  It is in this spirit that I am going to discuss one of my great loves: INTERCAL.
 
 > The names you are about to ignore are true. However, the story has been changed significantly. Any resemblance of the programming language portrayed here to other programming languages, living or dead, is purely coincidental.
 >
-> - INTERCAL-72 Language Specification, Introduction
+> \- INTERCAL-72 Language Specification, Introduction
 
 INTERCAL began one May morning as a joke between two residents of Princeton University, Donald R. Woods and James M. Lyon.  INTERCAL (affectionately expanded as the Compiler Language With No Pronounceable Acronym) was literally meant to be a compiler language that had nothing in common with any of the major languages of the period, eschewing everything but assignment statements, variables, arrays, and “IO”.  INTERCAL has no notion of pointers, strings, subroutines, boolean logic, and features a type system that’s little more than a sick joke wrapped in a cruel irony.  This strict adherence to a perverted definition of simplicity lead to a language innovative for its unconventional use of the ASCII character set, its nonsensical syntax, and its voluminous avenues for errors and confusion.  The INTERCAL-72 spec puts it best:
 
 > It has few capabilities, and thus there are few restrictions to be kept in mind. Since it is an exceedingly easy language to learn, one might expect it would be a good language for initiating novice programmers. Perhaps surprising, than, is the fact that it would be more likely to initiate a novice into a search for another line of work.
 >
-> - INTERCAL-72 Language Specification, Section 2.2
+> \- INTERCAL-72 Language Specification, Section 2.2
 
 ## Character Set
 
@@ -73,7 +73,7 @@ INTERCAL is built around the notion of optionally-numbered ordered statements co
 
 Line numbers must be explicitly reserved by the programmer or the library writer so as to avoid clashes with existing line numbers.  For example, the INTERCAL system libraries reside on line numbers 1000-1999, and any program making use of lines or labels in that range are subject to undefined behavior.  C-INTERCAL corrects this with the `-E` option, but this obstructs inclusion of the standard libraries).  Every line may be preceded by a double-oh-seven (%) and a number from 0-100 inclusive which indicates the probability a given line may be executed when it is reached, allowing some notion of randomness to any INTERCAL program.
 
-Variables must be of type spot, two-spot, tail, or hybrid, and may only be named numerically.  All INTERCAL variables are miniature stacks that can be pushed to with the `STASH` command, and popped off with the `RETRIEVE` command.  Attempting to `RETRIEVE` variables that have not been STASH'ed to results in the runtime error message `THROW STICK BEFORE RETRIEVING`.  Variables may also be IGNORE'd, that is made immune to the effects of any subsequent statements except the `REMEMBER` command, which annuls the effects of an `IGNORE`.  Assignment is acchieved through the use of the `<-` operator (pronounced calculate).  For example, to declare a 16-bit integer `.1` containing the value 16, then terminating, the following program could be written:
+Variables must be of type spot, two-spot, tail, or hybrid, and may only be named numerically.  All INTERCAL variables are miniature stacks that can be pushed to with the `STASH` command, and popped off with the `RETRIEVE` command.  Attempting to `RETRIEVE` variables that have not been `STASH`'ed to results in the runtime error message `THROW STICK BEFORE RETRIEVING`.  Variables may also be `IGNORE`'d, that is made immune to the effects of any subsequent statements except the `REMEMBER` command, which annuls the effects of an `IGNORE`.  Assignment is acchieved through the use of the `<-` operator (pronounced calculate).  For example, to declare a 16-bit integer `.1` containing the value 16, then terminating, the following program could be written:
 
 ```
 DO :1 <- #16
@@ -132,7 +132,7 @@ DO GIVE UP
 
 ## A Turing Tarpit
 
-INTERCAL is the original Turing Tarpit language.  That is, it is a language that is turing complete through the most ridiculous or useless constructs.  Bearing that in mind, INTERCAL's Turing Completeness stems from its `COME FROM` and `NEXT` statements.  `COME FROM` is really `GOTO` implemented "backwards in time", and thus can be used to insert certain statements after a line is executed.  For example:
+INTERCAL is the original Turing Tarpit language.  That is, it is a language that is Turing complete through the most ridiculous or useless constructs.  Bearing that in mind, INTERCAL's Turing-completeness stems from its `COME FROM` and `NEXT` statements.  `COME FROM` is really `GOTO` implemented "backwards in time", and thus can be used to insert certain statements after a line is executed.  For example:
 
 ```
 (1)     DO <any statement>
@@ -176,7 +176,8 @@ As noted by the spec:
 The input side of IO is accomplished through the `WRITE IN` statement.  Rather than restrict the types of the variables input will be written to, INTERCAL severely restricts the languages and forms of any input.  Numbers to be read in must be written out in all caps in english and the digits must be separated by at least one space.  For example, the range of 32-bit numbers is `ZERO`, or `OH` through `FOUR TWO NINE FOUR NINE SIX SEVEN TWO NINE FIVE`.  In an attempt to appear more "international", INTERCAL also recognizes input in the following languages: Sanskrit, Basque, Tagalog, Classical Nahuatl, Georgian, Kwakiutl, and Volapuk.  
 
 > Attempting to write in a value greater than or equal to `SIX FIVE FIVE THREE SIX` for a 16-bit variable will result in the error message, `DON'T BYTE OFF MORE THAN YOU CAN CHEW`.
-> - INTERCAL-72 Language Specification, Section 2.4.4.12
+>
+> \- INTERCAL-72 Language Specification, Section 2.4.4.12
 
 Output is acchieved through butchered Roman Numerals, which are as awful as the name implies.  Overlines indicate that the value below is to be multiplied by 1000, and lowercase letters indicate the value should be multiplied by 1000000.  Zero is represented by just an overline.  Thus, the constant `#6,000` is written out as:
 
@@ -184,18 +185,20 @@ Output is acchieved through butchered Roman Numerals, which are as awful as the 
 __
 VI
 ```
+
 ## It Gets So Very Much Worse
 
-INTERCAL also comes in a dialect known as TriNTERCAL, or Trinary INTERCAL.  Whereas classical INTERCAL concerns itself with mangling binary operators, TriNTERCAL deals with trits, or 3-bit values.  TriNTERCAL introduces two new operators, whirlpool (@) and sharkfin (^), and overloads What (?) to be substraction without borrowing.  Despite how complicated this all may sound, the TriNTERCAL spec actually presents it excellently using the metaphor of "choice" and "preference".  For example, binary `AND` is said to "prefer to returning 0, but returning 1 given no other choice", and binary OR is said to "prefer returning 1, but returning 0 given no other choice." Trinary `OR` means given 0, 1, 2, the operator prefers to return 2, then 0, and will return 1 given no other choice.  Trinary `AND` wants to return 0, will accept returning 1, and returns 2 given no other choice.  The only operator left is whirlpool, or logical `BUT`, which the spec puts best:
+INTERCAL also comes in a dialect known as TriNTERCAL, or Trinary INTERCAL.  Whereas classical INTERCAL concerns itself with mangling binary operators, TriNTERCAL deals with trits, or 3-bit values.  TriNTERCAL introduces two new operators, whirlpool (@) and sharkfin (^), and overloads What (?) to be substraction without borrowing.  Despite how complicated this all may sound, the TriNTERCAL spec actually presents it excellently using the metaphor of "choice" and "preference".  For example, binary `AND` is said to "prefer returning 0, but returning 1 given no other choice", and binary OR is said to "prefer returning 1, but returning 0 given no other choice." Trinary `OR` means given 0, 1, 2, the operator prefers to return 2, then 0, and will return 1 given no other choice.  Trinary `AND` wants to return 0, will accept returning 1, and returns 2 given no other choice.  The only operator left is whirlpool, or logical `BUT`, which the spec puts best:
 
 > In TriINTERCAL programs the whirlpool (@) denotes the unary tritwise BUT operation. You can think of the whirlpool as drawing values preferentially towards the central value 1. Alternatively, you can think of it as drawing your soul and your sanity inexorably down....
+>
+> \- INTERCAL-72 Language Specification, Section 6.5.2.1
 
 ## Finding Value Among Garbage
 
 If you've made it this far without either dying of laughter or gouging your eyeballs out or considering Office-Space'ing your computer at the stupidity of programming language jokes in the 70's, then there has to be a question as to why languages like INTERCAL have any value aside from demonstration purposes.  INTERCAL is not a language I would ever want to program in for client work, nor is a language that makes much in the way of useful work possible.  Where the value of esolangs lies is in strengthening the logical aspects of my mind.  INTERCAL and others like it force me to think about my code far more structurally than ever before, and logic must be inherent in the program itself and not datatypes, interfaces, or collections.  By lowering the amount of avenues and alternatives that I have to consider when trying to even get a program "running" in one of these languages, they have the effect of focusing my efforts rather than hindering them.
 
 One final note: For the love of god, don't specify the optimizer option when trying to compile INTERCAL.  The optimizer will run for hours or days at a time attempting to "optimize" (ha!) your code.  
-
 
 </p>
 |]
