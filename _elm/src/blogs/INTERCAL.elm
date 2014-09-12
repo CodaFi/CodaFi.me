6c55861a97a29da03ae20baccb4f6202cb0ad887
CodaFi <devteam.codafi@gmail.com>
2014-09-11 21:50:04 -0600

    Typos

--- _elm/src/blogs/INTERCAL.elm
+++ _elm/src/blogs/INTERCAL.elm
@@ -29,35 +29,35 @@ Character 	 Name                       	 Use (if any)
 ,         	 tail                       	 identify 16-bit array
 ;         	 hybrid                     	 identify 32-bit array
 #         	 mesh                       	 identify constant
-=         	 half-mesh                  	
+=         	 half-mesh
 '         	 spark                      	 grouper
-`         	 backspark                  	
+`         	 backspark
 !         	 wow                        	 equivalent to spark-spot
 ?         	 what                       	 unary XOR
 "         	 rabbit-ears                	 grouper
 ".       	 rabbit                     	 equivalent to ears-spot
-|         	 spike                      	
+|         	 spike
 %         	 double-oh-seven            	 percentage qualifier
 -         	 worm                       	 used with angles
 <         	 angle                      	 used with worms
->         	 right angle                	
+>         	 right angle
 (         	 wax                        	 precedes line label
 )         	 wane                       	 follows line label
-[         	 U turn                     	
-]         	 U turn back                	
-{         	 embrace                    	
-}         	 bracelet                   	
+[         	 U turn
+]         	 U turn back
+{         	 embrace
+}         	 bracelet
 &         	 ampersand *                	 unary AND
 V         	 V (or book)                	 unary OR
 $         	 big money                  	 binary interleave
 ¢         	 change                     	 binary interleave
 ~         	 sqiggle                    	 binary select
-_         	 flat worm                  	
+_         	 flat worm
 ¯         	 overline                   	 indicates "times 1000"
 +         	 intersection               	 separates list items
-/         	 slat                       	
-\         	 backslat                   	
-¬         	 hookworm                   	             	
+/         	 slat
+\         	 backslat
+¬         	 hookworm
 ```
 
 ## Structure
@@ -82,7 +82,7 @@ DO GIVE UP
 
 ## Operators
 
-INTERCAL-72 includes 5 operators, of which 2 are binary and 3 are unary.  While these operators may share names with many common operators in other languages, they have literally nothing in common with them, and often perform unnecessarily complex mathematical operations on the bits of a variable to arrive at an answer.  The binary operators, mingle, represented by big-money ($) and select, represented by a worm (~).  Mingle takes the bits of two numbers, interleaves them, then returns the result with any overflow than doesn't fit into the type of the second variable being lopped off, or causing a runtime error (whichever the compiler finds most convenient).  It's always best to visualize this operator, so here is 22 being mingled with 8:
+INTERCAL-72 includes 5 operators, of which 2 are binary and 3 are unary.  While these operators may share names with many common operators in other languages, they have literally nothing in common with them, and often perform unnecessarily complex mathematical operations on the bits of a variable to arrive at an answer.  The binary operators are mingle, represented by big-money ($) and select, represented by a worm (~).  Mingle takes the bits of two numbers, interleaves them, then returns the result with any overflow than doesn't fit into the type of the second variable being lopped off, or causing a runtime error (whichever the compiler finds most convenient).  It's always best to visualize this operator, so here is 22 being mingled with 8:
 
 ```
 DO .1 <- #22$#8
@@ -107,9 +107,9 @@ _______________
 
 Brilliant.  This isn't so hard to deal with in practice, it's just quite a bit more math than it should be.  Now for the Unary Operators:
 
-INTERCAL's unary operators work in much the same way its binary operators do.  Bit manipulation is the name of the game when dealing with unary operators, and INTERCAL cares so much about its unary operators that they may actually be used in succession.  The only downside is the restrictions on where these operators may be used: after the type of a variable in its declarations. 
+INTERCAL's unary operators work in much the same way its binary operators do.  Bit manipulation is the name of the game when dealing with unary operators, and INTERCAL cares so much about its unary operators that they may actually be used in succession.  The only downside is the restrictions on where these operators may be used: after the type of a variable in its declarations.
 
-The Ampersand (curiously, the only character that retains its proper name) goes through each pair of bits in a number, `AND`s them together, the Book (V) operator performs logical `OR`'ing of the bits of a number, and What (?) performs logical XOR'ing of the bits of a number. 
+The Ampersand (curiously, the only character that retains its proper name) goes through each pair of bits in a number, `AND`s them together, the Book (V) operator performs logical `OR`'ing of the bits of a number, and What (?) performs logical XOR'ing of the bits of a number.
 
 ```
 DO .1 <- #&100
@@ -148,7 +148,7 @@ should be thought of as being equivalent to
         .
         .
         .
-(3)     DO NOTHING  
+(3)     DO NOTHING
 ```
 
 Of course, the 2nd part of the example above is flawed, in that GOTO is not an operator in INTERCAL, and it will not compile.
@@ -171,9 +171,9 @@ As noted by the spec:
 > `DO ABSTAIN FROM ABSTAINING` is perfectly valid, as is `DO ABSTAIN FROM REINSTATING` (although this latter is not usually recommended). However, the statement `DO ABSTAIN FROM GIVING UP` is not accepted, even though `DON'T GIVE UP` is.
 
 
-## "IO" 
+## "IO"
 
-The input side of IO is accomplished through the `WRITE IN` statement.  Rather than restrict the types of the variables input will be written to, INTERCAL severely restricts the languages and forms of any input.  Numbers to be read in must be written out in all caps in english and the digits must be separated by at least one space.  For example, the range of 32-bit numbers is `ZERO`, or `OH` through `FOUR TWO NINE FOUR NINE SIX SEVEN TWO NINE FIVE`.  In an attempt to appear more "international", INTERCAL also recognizes input in the following languages: Sanskrit, Basque, Tagalog, Classical Nahuatl, Georgian, Kwakiutl, and Volapuk.  
+The input side of IO is accomplished through the `WRITE IN` statement.  Rather than restrict the types of the variables input will be written to, INTERCAL severely restricts the languages and forms of any input.  Numbers to be read in must be written out in all caps in english and the digits must be separated by at least one space.  For example, the range of 32-bit numbers is `ZERO`, or `OH` through `FOUR TWO NINE FOUR NINE SIX SEVEN TWO NINE FIVE`.  In an attempt to appear more "international", INTERCAL also recognizes input in the following languages: Sanskrit, Basque, Tagalog, Classical Nahuatl, Georgian, Kwakiutl, and Volapuk.
 
 > Attempting to write in a value greater than or equal to `SIX FIVE FIVE THREE SIX` for a 16-bit variable will result in the error message, `DON'T BYTE OFF MORE THAN YOU CAN CHEW`.
 >
@@ -198,7 +198,7 @@ INTERCAL also comes in a dialect known as TriNTERCAL, or Trinary INTERCAL.  Wher
 
 If you've made it this far without either dying of laughter or gouging your eyeballs out or considering Office-Space'ing your computer at the stupidity of programming language jokes in the 70's, then there has to be a question as to why languages like INTERCAL have any value aside from demonstration purposes.  INTERCAL is not a language I would ever want to program in for client work, nor is a language that makes much in the way of useful work possible.  Where the value of esolangs lies is in strengthening the logical aspects of my mind.  INTERCAL and others like it force me to think about my code far more structurally than ever before, and logic must be inherent in the program itself and not datatypes, interfaces, or collections.  By lowering the amount of avenues and alternatives that I have to consider when trying to even get a program "running" in one of these languages, they have the effect of focusing my efforts rather than hindering them.
 
-One final note: For the love of god, don't specify the optimizer option when trying to compile INTERCAL.  The optimizer will run for hours or days at a time attempting to "optimize" (ha!) your code.  
+One final note: For the love of god, don't specify the optimizer option when trying to compile INTERCAL.  The optimizer will run for hours or days at a time attempting to "optimize" (ha!) your code.
 
 </p>
 |]
